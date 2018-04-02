bba6c8a8146502bd573e7e254903ced66dd884cb
Edgaras Jovaiša <edgaras@adeoweb.biz>
2018-04-02 15:13:03 +0300

    grunt, npm

---
+++ gruntfile.js
@@ -0,0 +1,114 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            dist: {
+                options: {
+                    sourceMap: true,
+                    outputStyle: 'nested'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'sass/build/',
+                    src: ['*.scss'],
+                    dest: 'build/css/',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        postcss: {
+            dist: {
+                options: {
+                    map: true,
+                    processors: [
+                        require('autoprefixer')
+                    ]
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'build/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'build/css/',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        cmq: {
+            dist: {
+                options: {
+                    log: true,
+                    ext: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'build/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'build/css/',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        cssmin: {
+            dist: {
+                options: {
+                    processImport: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'build/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'build/css/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        watch: {
+            scss: {
+                options: {
+                    livereload: false,
+                    spawn: true,
+                    interval: 5007
+                },
+                files: ['sass/**/*.scss'],
+                tasks: ['sass', 'postcss:dist']
+            },
+            css: {
+                options: {
+                    livereload: true,
+                    spawn: true,
+                    interval: 5007
+                },
+                files: ['build/css/*.css']
+            },
+            js: {
+                options: {
+                    livereload: true,
+                    interval: 5007
+                },
+                files: ['js/*.js']
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-combine-media-queries');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['sass', 'cmq', 'postcss:dist', 'cssmin']);
+    grunt.registerTask('buildcss', ['sass', 'cmq', 'postcss:dist', 'cssmin']);
+
+}
